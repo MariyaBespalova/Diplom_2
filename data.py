e0676c8848ee1345b8c75b8e72135a8bc41781b8
MariyaBespalova <bespalovamaschka@yandex.ru>
2025-11-16 12:19:39 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,12 +1,3 @@
-class Url:
-    BASE_URL = 'https://stellarburgers.education-services.ru/'
-    CREATED_USER = '/api/auth/register'
-    LOGIN_USER = '/api/auth/login'
-    UPDATE_USER = '/api/auth/user'
-    CREATED_ORDER = '/api/orders'
-    GET_ORDERS_USER = '/api/orders'
-    DELETE_USER = '/api/auth/user'
-
 class Ingredients:
     BODY_WITHOUT_INGREDIENTS = { 'ingredients': []}
     BODY_INVALID_HASH_INGREDIENT = {'ingredients': ['61c0c5a71d1f82001bdaaa6c1']}
@@ -25,3 +16,16 @@ class DataResponse:
     RECEIVING_ORDERS_AUTHORIZED_USER = {"success": False,"message": "You should be authorised"}
     AUTHORIZATION_WITH_INCORRECT_USERNAME_AND_PASSWORD = {"success": False,"message": "email or password are incorrect"}
     UPDATE_DATA_WITHOUT_AUTHORIZATION_USER = {"success": False,"message": "You should be authorised"}
+
+class Data:
+    USER_CREATION_DATA = [
+        ("", 555555, "serega"),
+        ("qatest-11@yandex.ru", "", "serega"),
+        ("qatest-11@yandex.ru", 555555, "")
+    ]
+
+    EXPECTED_RESPONSE_BODY = {
+        "message": "Заполните все обязательные поля",
+        "code": 403,
+        "type": "Bad Request"
+    }
