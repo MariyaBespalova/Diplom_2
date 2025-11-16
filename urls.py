095c0f6f3478654b15c4855ad0b6ddfe8baecd22
MariyaBespalova <bespalovamaschka@yandex.ru>
2025-11-16 12:21:44 +0300

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,8 @@
+class Url:
+    BASE_URL = 'https://stellarburgers.education-services.ru/'
+    CREATED_USER = '/api/auth/register'
+    LOGIN_USER = '/api/auth/login'
+    UPDATE_USER = '/api/auth/user'
+    CREATED_ORDER = '/api/orders'
+    GET_ORDERS_USER = '/api/orders'
+    DELETE_USER = '/api/auth/user'
